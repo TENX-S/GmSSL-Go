cec01606a1bbb4d97e8953a36705dea0ef2b5345
TENX-S <ttenx@pm.me>
2024-05-12 12:34:50 +0800

    chore: 更新 gcc 到 13，添加windows/386静态库
    Signed-off-by: TENX-S <ttenx@pm.me>

--- build.go
+++ build.go
@@ -16,6 +16,7 @@ package gmssl
 #cgo linux,amd64 LDFLAGS: -L./lib/linux-amd64 -lgmssl
 #cgo linux,arm64 LDFLAGS: -L./lib/linux-arm64 -lgmssl
 #cgo linux,arm LDFLAGS: -L./lib/linux-armv7 -lgmssl
+#cgo windows,386 LDFLAGS: -L./lib/windows-386 -lgmssl -lws2_32
 #cgo windows,amd64 LDFLAGS: -L./lib/windows-amd64 -lgmssl -lws2_32
 */
 import "C"
